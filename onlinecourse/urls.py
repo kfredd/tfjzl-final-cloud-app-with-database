da19fdad8a696937aa5add8d631f8b5c4124c94f
kfredd <juniorkuffour69@gmail.com>
2024-11-21 00:48:53 -0500

    changes made from the lab environment

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -5,8 +5,6 @@ from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
     # name the URL
     path(route='', view=views.CourseListView.as_view(), name='index'),
     path('registration/', views.registration_request, name='registration'),
@@ -18,7 +16,7 @@ urlpatterns = [
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
     # <HINT> Create a route for submit view
-
+    path('<int:course_id>/submit/', views.submit, name="submit"),
     # <HINT> Create a route for show_exam_result view
-
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
